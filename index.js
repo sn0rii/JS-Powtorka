0017fda7b6075ade64f011b5aebaabfd11006e23
Wojtek M <ojciecneo@gmail.com>
2023-07-19 07:00:22 +0200

    if statements nested

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 let name = "Wojtek";
 console.log(name.charAt(Math.floor(Math.random() * name.length)));
+
+// IF STATEMENTS
+
+let testScore = 59;
+let collegeStudent = true;
+let grade;
+
+if (testScore >= 90) {
+  grade = "A";
+} else if (testScore >= 80) {
+  grade = "B";
+} else if (testScore >= 70) {
+  grade = "C";
+} else if (testScore >= 60) {
+  grade = "D";
+} else {
+  if (collegeStudent) {
+    grade = "U";
+  } else {
+    grade = "F";
+  }
+}
+
+console.log(grade);
