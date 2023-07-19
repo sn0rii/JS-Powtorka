4b5ab8e7a2b449fb1891065030bc14dba7e5f534
Wojtek M <ojciecneo@gmail.com>
2023-07-19 07:07:54 +0200

    ROCK AND PAPER v1

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-let name = "Wojtek";
-console.log(name.charAt(Math.floor(Math.random() * name.length)));
+// let name = "Wojtek";
+// console.log(name.charAt(Math.floor(Math.random() * name.length)));
 
-// IF STATEMENTS
+// // IF STATEMENTS
 
-let testScore = 59;
-let collegeStudent = true;
-let grade;
+// let testScore = 59;
+// let collegeStudent = true;
+// let grade;
 
-if (testScore >= 90) {
-  grade = "A";
-} else if (testScore >= 80) {
-  grade = "B";
-} else if (testScore >= 70) {
-  grade = "C";
-} else if (testScore >= 60) {
-  grade = "D";
-} else {
-  if (collegeStudent) {
-    grade = "U";
+// if (testScore >= 90) {
+//   grade = "A";
+// } else if (testScore >= 80) {
+//   grade = "B";
+// } else if (testScore >= 70) {
+//   grade = "C";
+// } else if (testScore >= 60) {
+//   grade = "D";
+// } else {
+//   if (collegeStudent) {
+//     grade = "U";
+//   } else {
+//     grade = "F";
+//   }
+// }
+
+// console.log(grade);
+
+// ROCK AND PAPER
+let playerOne = "rock";
+let computer = "paper";
+
+if (playerOne === computer) {
+  //tie game
+} else if (playerOne === "rock") {
+  if (computer === "paper") {
+    //computer win
   } else {
-    grade = "F";
+    // playerOne wins
+  }
+} else if (playerOne === "paper") {
+  if (computer === "scissors") {
+    //computer win
+  } else {
+    // playerOne wins
+  }
+} else {
+  if (computer === "rock") {
+    // computer wins
+  } else {
+    // player wins
   }
 }
-
-console.log(grade);
