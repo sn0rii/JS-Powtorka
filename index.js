bb8dbaa5dafc4596afc943465cb1fe8aa32666bc
Wojtek M <ojciecneo@gmail.com>
2023-07-20 07:56:20 +0200

    ternary operator

--- index.js
+++ index.js
@@ -69,31 +69,42 @@
 //     console.log("No match");
 // }
 
-let playerOne = "scissors";
-let computer = "paper";
+// let playerOne = "scissors";
+// let computer = "paper";
 
-switch (playerOne) {
-  case computer:
-    console.log("Tie game!");
-    break;
-  case "rock":
-    if (computer === "paper") {
-      console.log("computer wins!");
-    } else {
-      console.log("playerOne wins!");
-    }
-    break;
-  case "paper":
-    if (computer === "scissors") {
-      console.log("computer wins");
-    } else {
-      console.log("playerOne wins!");
-    }
-    break;
-  default:
-    if (computer === "rock") {
-      console.log("computer wins");
-    } else {
-      console.log("playerOne wins!");
-    }
-}
+// switch (playerOne) {
+//   case computer:
+//     console.log("Tie game!");
+//     break;
+//   case "rock":
+//     if (computer === "paper") {
+//       console.log("computer wins!");
+//     } else {
+//       console.log("playerOne wins!");
+//     }
+//     break;
+//   case "paper":
+//     if (computer === "scissors") {
+//       console.log("computer wins");
+//     } else {
+//       console.log("playerOne wins!");
+//     }
+//     break;
+//   default:
+//     if (computer === "rock") {
+//       console.log("computer wins");
+//     } else {
+//       console.log("playerOne wins!");
+//     }
+// }
+
+//TERNARY OPERATOR
+
+let soup = "Chicken ramen";
+let isCustomerBanned = false;
+let soupAccess = isCustomerBanned
+  ? "Sorry, no soup for you!"
+  : soup
+  ? `Yes we have ${soup} today.`
+  : "Sorry, no soup today.";
+console.log(soupAccess);
