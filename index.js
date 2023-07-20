f4cb88a0c89f6ec79031e691806b8027c71888a3
Wojtek M <ojciecneo@gmail.com>
2023-07-20 08:05:20 +0200

    ternary operator - Game update

--- index.js
+++ index.js
@@ -100,11 +100,39 @@
 
 //TERNARY OPERATOR
 
-let soup = "Chicken ramen";
-let isCustomerBanned = false;
-let soupAccess = isCustomerBanned
-  ? "Sorry, no soup for you!"
-  : soup
-  ? `Yes we have ${soup} today.`
-  : "Sorry, no soup today.";
-console.log(soupAccess);
+// let soup = "Chicken ramen";
+// let isCustomerBanned = false;
+// let soupAccess = isCustomerBanned
+//   ? "Sorry, no soup for you!"
+//   : soup
+//   ? `Yes we have ${soup} today.`
+//   : "Sorry, no soup today.";
+// console.log(soupAccess);
+
+// let testScore = 90;
+// let myGrade =
+//   testScore > 89
+//     ? "A"
+//     : testScore > 79
+//     ? "B"
+//     : testScore > 69
+//     ? "C"
+//     : testScore > 59
+//     ? "D"
+//     : "F";
+// console.log(`My test grade is a ${myGrade}.`);
+
+let playerOne = "scissors";
+let computer = "paper";
+
+let result =
+  playerOne === computer
+    ? "Tie game!"
+    : playerOne === "rock" && computer === "paper"
+    ? "Computer wins!"
+    : playerOne === "paper" && computer === "scissors"
+    ? "Computer wins!"
+    : playerOne === "scissors" && computer === "rock"
+    ? "Computer wins!"
+    : "playerOne wins !";
+console.log(result);
