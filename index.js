f7a06f2873eb2d1460fbfee817cda16f7170d688
Wojtek M <ojciecneo@gmail.com>
2023-07-20 07:37:19 +0200

    switch statement - game update

--- index.js
+++ index.js
@@ -52,16 +52,48 @@
 // }
 
 // SWITCH STATEMENT
-switch ("2") {
-  case 1:
-    break;
+// switch (Math.floor(Math.random() * 5 + 1)) {
+//   case 1:
+//     console.log(1);
+//     break;
 
-  case 2:
-    break;
+//   case 2:
+//     console.log(2);
+//     break;
 
-  case 3:
-    break;
+//   case 3:
+//     console.log(3);
+//     break;
 
+//   default:
+//     console.log("No match");
+// }
+
+let playerOne = "scissors";
+let computer = "paper";
+
+switch (playerOne) {
+  case computer:
+    console.log("Tie game!");
+    break;
+  case "rock":
+    if (computer === "paper") {
+      console.log("computer wins!");
+    } else {
+      console.log("playerOne wins!");
+    }
+    break;
+  case "paper":
+    if (computer === "scissors") {
+      console.log("computer wins");
+    } else {
+      console.log("playerOne wins!");
+    }
+    break;
   default:
-    console.log("No match");
+    if (computer === "rock") {
+      console.log("computer wins");
+    } else {
+      console.log("playerOne wins!");
+    }
 }
