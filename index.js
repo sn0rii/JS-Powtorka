c929a4665e6a85e1655cd9b8a871dcb3cb1cdd67
Wojtek M <ojciecneo@gmail.com>
2023-07-22 12:03:49 +0200

    for loop

--- index.js
+++ index.js
@@ -195,11 +195,28 @@
 // }
 
 // loops
-let myNumber = 50;
-while (myNumber < 50) {
-  myNumber += 2;
-  console.log(myNumber);
-}
-do {
-  console.log(myNumber);
-} while (myNumber < 50);
+
+// let myNumber = 50;
+// while (myNumber < 50) {
+//   myNumber += 2;
+//   console.log(myNumber);
+// }
+// do {
+//   console.log(myNumber);
+// } while (myNumber < 50);
+
+// let name = "Wojtek";
+// for (let i = 0; i < name.length; i++) {
+//   console.log(name.charAt(i));
+// }
+
+// let counter = 0;
+// let myLetter;
+// while (true) {
+//   myLetter = name[counter];
+//   console.log(myLetter);
+//   if (myLetter === "e") break;
+//   counter++;
+// }
+
+// FUNCTION //
