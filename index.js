8bd02436d275ba62343ce4b4c8f732526c1df2f8
Wojtek M <ojciecneo@gmail.com>
2023-07-22 11:53:45 +0200

    do while loop

--- index.js
+++ index.js
@@ -195,8 +195,11 @@
 // }
 
 // loops
-let myNumber = 0;
+let myNumber = 50;
 while (myNumber < 50) {
   myNumber += 2;
   console.log(myNumber);
 }
+do {
+  console.log(myNumber);
+} while (myNumber < 50);
