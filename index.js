b83430b1fe84509556e44e2599da6da89a6be0c8
Wojtek M <ojciecneo@gmail.com>
2023-07-22 11:50:26 +0200

    while loop

--- index.js
+++ index.js
@@ -150,44 +150,53 @@
 //   console.log("You didn't enter your name ");
 // }
 
-let playGame = confirm("Shall we play rock, paper, or scissors?");
-if (playGame) {
-  // play
-  let playerChoice = prompt("Please enter rock, paper, or scissors.");
-  if (playerChoice) {
-    let playerOne = playerChoice.trim().toLocaleLowerCase();
-    if (
-      playerOne === "rock" ||
-      playerOne == "paper" ||
-      playerOne === "scissors"
-    ) {
-      let computerChoice = Math.floor(Math.random() * 3 + 1);
-      let computer =
-        computerChoice === 1
-          ? "rock"
-          : computerChoice === 2
-          ? "paper"
-          : "scissors";
+// ROCK PAPER SCISSORS GAME ////
 
-      let result =
-        playerOne === computer
-          ? "Tie game!"
-          : playerOne === "rock" && computer === "paper"
-          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-          : playerOne === "paper" && computer === "scissors"
-          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-          : playerOne === "scissors" && computer === "rock"
-          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-          : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
-      alert(result);
-      let playAgain = confirm("Play Again?");
-      playAgain ? location.reload() : alert("Ok, thanks for playing.");
-    } else {
-      alert("You didn't enter rock, paper, or scissors.");
-    }
-  } else {
-    alert("I guess you changed your mind. Maybe next time.");
-  }
-} else {
-  alert("Ok, maybe next time.");
+// let playGame = confirm("Shall we play rock, paper, or scissors?");
+// if (playGame) {
+//   // play
+//   let playerChoice = prompt("Please enter rock, paper, or scissors.");
+//   if (playerChoice) {
+//     let playerOne = playerChoice.trim().toLocaleLowerCase();
+//     if (
+//       playerOne === "rock" ||
+//       playerOne == "paper" ||
+//       playerOne === "scissors"
+//     ) {
+//       let computerChoice = Math.floor(Math.random() * 3 + 1);
+//       let computer =
+//         computerChoice === 1
+//           ? "rock"
+//           : computerChoice === 2
+//           ? "paper"
+//           : "scissors";
+
+//       let result =
+//         playerOne === computer
+//           ? "Tie game!"
+//           : playerOne === "rock" && computer === "paper"
+//           ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//           : playerOne === "paper" && computer === "scissors"
+//           ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//           : playerOne === "scissors" && computer === "rock"
+//           ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+//           : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+//       alert(result);
+//       let playAgain = confirm("Play Again?");
+//       playAgain ? location.reload() : alert("Ok, thanks for playing.");
+//     } else {
+//       alert("You didn't enter rock, paper, or scissors.");
+//     }
+//   } else {
+//     alert("I guess you changed your mind. Maybe next time.");
+//   }
+// } else {
+//   alert("Ok, maybe next time.");
+// }
+
+// loops
+let myNumber = 0;
+while (myNumber < 50) {
+  myNumber += 2;
+  console.log(myNumber);
 }
