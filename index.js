9881290c06f2ca528fd449d672829bc0621fe097
Wojtek M <ojciecneo@gmail.com>
2023-07-28 06:50:17 +0200

    second fetch parameter

--- index.js
+++ index.js
@@ -348,19 +348,34 @@
 
 // ASYNC JS //
 
-const getAllEmails = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const jsonUserData = await response.json();
+// const getAllEmails = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const jsonUserData = await response.json();
 
-  const userEmail = jsonUserData.map((user) => {
-    return user.email;
+//   const userEmail = jsonUserData.map((user) => {
+//     return user.email;
+//   });
+
+//   postToWebPage(userEmail);
+// };
+
+// postToWebPage = (data) => {
+//   console.log(data);
+// };
+
+// getAllEmails();
+
+const getDadJoke = async () => {
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
   });
 
-  postToWebPage(userEmail);
+  const jsonJokeData = await response.json();
+
+  console.log(jsonJokeData);
 };
 
-postToWebPage = (data) => {
-  console.log(data);
-};
-
-getAllEmails();
+getDadJoke();
