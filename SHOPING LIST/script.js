701629d32a14ebe7aaba8c9bfaafb170244a76f5
Wojtek Mitan <wojtekmitan91@gmail.com>
2023-07-29 17:28:09 +0200

    Object.values

--- SHOPING LIST/script.js
+++ SHOPING LIST/script.js
@@ -47,10 +47,49 @@
 // query selector all
 
 const listItem = document.querySelectorAll(".item");
-console.log(listItem[1].innerText);
+// console.log(listItem[1].innerText);
 
 // listItem[1].style.color = "red";
 
-listItem.forEach((item) => {
-  item.style.color = "red";
+// listItem.forEach((item) => {
+//   item.style.color = "red";
+// });
+
+// listItem.forEach((item, index) => {
+//   item.style.color = "red";
+
+//   if (index === 1) {
+//     item.remove();
+//   }
+
+//   if (index === 0) {
+//     item.innerHTML = `Oranges
+//           <button class="remove-item btn-link text-red">
+//             <i class="fa-solid fa-xmark"></i>`;
+//   }
+// });
+
+const listItems2 = document.getElementsByClassName("item");
+
+// console.log(listItems2[2].innerText);
+
+const listItemsArray = Array.from(listItems2);
+
+// console.log(listItemsArray);
+
+listItemsArray.forEach((item) => {
+  console.log(item.innerText);
 });
+
+const listItems3 = document.getElementsByTagName("li");
+console.log(listItems3[0]);
+
+// const witek = {
+//   name: "wojtek",
+//   nazwisko: "klops",
+// };
+
+// console.log(witek);
+
+// const wojtekLista = Object.values(witek);
+// console.log(wojtekLista);
