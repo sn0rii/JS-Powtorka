6e84cb10a9f89163365796957f4b186f4673d171
Wojtek M <ojciecneo@gmail.com>
2023-07-29 08:53:54 +0200

    new chapter - js DOM

---
+++ SHOPING LIST/script.js
@@ -0,0 +1,45 @@
+// let output;
+
+// output = document.all;
+
+// console.log(output);
+
+// console.log(document.getElementById("app-title"));
+
+// SET ATTRIBUTES //
+
+document.getElementById("app-title").title = "Shopping List";
+document.getElementById("app-title").setAttribute("class", "title");
+
+const title = document.getElementById("app-title");
+
+// console.log(title.textContent);
+title.textContent = " Hello Man";
+title.innerText = "Hello Again";
+title.innerHTML = "<strong>Shopping List</strong>";
+
+// CHANGE STYLES
+
+title.style.color = "red";
+title.style.backgroundColor = "black";
+title.style.padding = "10px";
+title.style.borderRadius = "10px";
+
+// document.querySelector()
+
+console.log(document.querySelector("h1"));
+console.log(document.querySelector("#app-title"));
+console.log(document.querySelector(".container"));
+console.log(document.querySelector("input[type=text"));
+console.log(document.querySelector("li:nth-child(2").innerText);
+
+const secondItem = document.querySelector("li:nth-child(2");
+secondItem.innerText = "Apple juice";
+secondItem.style.color = "green";
+
+// use these methods on other elements
+
+const list = document.querySelector("ul");
+console.log(list);
+const firstItem = list.querySelector("li");
+firstItem.style.color = "red";
--- index.js
+++ index.js
@@ -401,35 +401,37 @@
 
 // postData(jokeObject);
 
-const getDataFromForm = () => {
-  const requestObj = {
-    firstName: "Bruce",
-    lastName: "Lee",
-    categories: ["nerdy"],
-  };
-  return requestObj;
-};
+// const getDataFromForm = () => {
+//   const requestObj = {
+//     firstName: "Bruce",
+//     lastName: "Lee",
+//     categories: ["nerdy"],
+//   };
+//   return requestObj;
+// };
 
-const buildRequestUrl = (requestData) => {
-  return `http://api.icndb.com/jokes/random?fristName=${requestData.firstName}&lastName=${requestData.lastName}`;
-};
+// const buildRequestUrl = (requestData) => {
+//   return `http://api.icndb.com/jokes/random?fristName=${requestData.firstName}&lastName=${requestData.lastName}`;
+// };
 
-const requestJoke = async (url) => {
-  const response = await fetch(url);
-  const jsonResponse = await response.json();
-  const joke = jsonResponse.value.joke;
-  postJokeToPage(joke);
-};
+// const requestJoke = async (url) => {
+//   const response = await fetch(url);
+//   const jsonResponse = await response.json();
+//   const joke = jsonResponse.value.joke;
+//   postJokeToPage(joke);
+// };
 
-const postJokeToPage = (joke) => {
-  console.log(joke);
-};
+// const postJokeToPage = (joke) => {
+//   console.log(joke);
+// };
 
-const processJokeRequest = async () => {
-  const requestData = getDataFromForm();
-  const requestUrl = buildRequestUrl(requestData);
-  await requestJoke(requestUrl);
-  console.log("finished!");
-};
+// const processJokeRequest = async () => {
+//   const requestData = getDataFromForm();
+//   const requestUrl = buildRequestUrl(requestData);
+//   await requestJoke(requestUrl);
+//   console.log("finished!");
+// };
 
-processJokeRequest();
+// processJokeRequest();
+
+// DOM TRAVIS
