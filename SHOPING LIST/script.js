fa39e8e410f9767b9bcd7be2ef9c07817fe60748
Wojtek M <ojciecneo@gmail.com>
2023-07-31 06:36:31 +0200

    clean version v2

--- SHOPING LIST/script.js
+++ SHOPING LIST/script.js
@@ -107,34 +107,63 @@ const listItem = document.querySelectorAll(".item");
 
 // console.log(div);
 
-function createListItem(item) {
-  const li = document.createElement("li");
+// function createListItem(item) {
+//   const li = document.createElement("li");
 
-  li.innerHTML = `${item}
-          <button class="remove-item btn-link text-red">
-            <i class="fa-solid fa-xmark"></i>
-          </button>`;
+//   li.innerHTML = `${item}
+//           <button class="remove-item btn-link text-red">
+//             <i class="fa-solid fa-xmark"></i>
+//           </button>`;
 
-  document.querySelector(".items").appendChild(li);
-}
+//   document.querySelector(".items").appendChild(li);
+// }
 
 //CLEAN VERSION //
+
+// function createNewItem(item) {
+//   const li = document.createElement("li");
+//   li.appendChild(document.createTextNode(item));
+
+//   const button = document.createElement("button");
+//   button.className = "remove-item btn-link text-red";
+
+//   const icon = document.createElement("i");
+//   icon.className = "fa-solid fa-xmark";
+
+//   button.appendChild(icon);
+//   li.appendChild(button);
+
+//   document.querySelector(".items").appendChild(li);
+// }
+
+// createListItem("Eggs");
+// createNewItem("Cheese");
 
 function createNewItem(item) {
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(item));
 
-  const button = document.createElement("button");
-  button.className = "remove-item btn-link text-red";
+  const button = createButton("remove-item btn-link text-red");
 
-  const icon = document.createElement("i");
-  icon.className = "fa-solid fa-xmark";
-
-  button.appendChild(icon);
   li.appendChild(button);
 
   document.querySelector(".items").appendChild(li);
+
+  function createButton(classes) {
+    const button = document.createElement("button");
+    button.className = classes;
+
+    const icon = createIcon("fa-solid fa-xmark");
+    button.appendChild(icon);
+
+    return button;
+  }
+  function createIcon(classes) {
+    const icon = document.createElement("i");
+    icon.className = classes;
+    return icon;
+  }
 }
 
-createListItem("Eggs");
 createNewItem("Cheese");
+createNewItem("Sausage");
