3e19a24d9ea479f19a02298f0f089361e25365d1
Wojtek M <ojciecneo@gmail.com>
2023-07-31 06:25:08 +0200

    clean version

--- SHOPING LIST/script.js
+++ SHOPING LIST/script.js
@@ -94,15 +94,47 @@ const listItem = document.querySelectorAll(".item");
 // const wojtekLista = Object.values(witek);
 // console.log(wojtekLista);
 
-const div = document.createElement("div");
-div.className = "my-element";
-div.id = "my-element";
-div.setAttribute("title", "My Element");
+// const div = document.createElement("div");
+// div.className = "my-element";
+// div.id = "my-element";
+// div.setAttribute("title", "My Element");
 
-// div.innerText = "Hello World";
-const text = document.createTextNode("Hello 2");
-div.appendChild(text);
+// // div.innerText = "Hello World";
+// const text = document.createTextNode("Hello 2");
+// div.appendChild(text);
 
-document.querySelector("ul").appendChild(div);
+// document.querySelector("ul").appendChild(div);
 
-console.log(div);
+// console.log(div);
+
+function createListItem(item) {
+  const li = document.createElement("li");
+
+  li.innerHTML = `${item}
+          <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+          </button>`;
+
+  document.querySelector(".items").appendChild(li);
+}
+
+//CLEAN VERSION //
+
+function createNewItem(item) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement("button");
+  button.className = "remove-item btn-link text-red";
+
+  const icon = document.createElement("i");
+  icon.className = "fa-solid fa-xmark";
+
+  button.appendChild(icon);
+  li.appendChild(button);
+
+  document.querySelector(".items").appendChild(li);
+}
+
+createListItem("Eggs");
+createNewItem("Cheese");
