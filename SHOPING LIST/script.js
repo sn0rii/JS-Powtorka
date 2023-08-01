2b0f075288bee4cf06643bbbf0eca187bb5496ef
Wojtek Mitan <wojtekmitan91@gmail.com>
2023-08-01 13:35:59 +0200

    mouse event

--- SHOPING LIST/script.js
+++ SHOPING LIST/script.js
@@ -269,16 +269,16 @@
 
 ///////
 
-const clearBtn = document.querySelector("#clear");
+// const clearBtn = document.querySelector("#clear");
 
-function onClear() {
-  const itemList = document.querySelector("ul");
-  const items = document.querySelectorAll("li");
+// function onClear() {
+//   const itemList = document.querySelector("ul");
+//   const items = document.querySelectorAll("li");
 
-  //   itemList.innerHTML = "";
+//   //   itemList.innerHTML = "";
 
-  items.forEach((item) => item.remove());
-}
+//   items.forEach((item) => item.remove());
+// }
 
 // JS event listener
 
@@ -292,8 +292,44 @@ function onClear() {
 
 // clearBtn.addEventListener("click", () => console.log("clear list"));
 
-clearBtn.addEventListener("click", onClear);
+// clearBtn.addEventListener("click", onClear);
 
 // setTimeout(() => clearBtn.removeEventListener("click", onClear), 5000);
 
 // setTimeout(() => clearBtn.click(), 5000);
+
+// const logo = document.querySelector("img");
+
+// const onClick = () => console.log("click");
+// const onDoubleClick = () => {
+//   if (document.body.style.backgroundColor !== "red") {
+//     document.body.style.backgroundColor = "red";
+//     document.body.style.color = "white";
+//   } else {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = "black";
+//   }
+// };
+// const onRightClick = () => console.log("right click event");
+
+// const onMouseDown = () => console.log("mouse down event");
+// const onMouseUp = () => console.log("mouse up event");
+// const onMouseWheel = () => console.log("mouse wheel event");
+// const onMouseOver = () => console.log("mouse over event");
+// const onMouseOut = () => console.log("mouse out event");
+// const onDragStart = () => console.log(" drag start event");
+// const onDrag = () => console.log(" drag event");
+// const onDragEnd = () => console.log(" drag end event");
+
+// // event listener
+// logo.addEventListener("click", onClick);
+// logo.addEventListener("dblclick", onDoubleClick);
+// logo.addEventListener("contextmenu", onRightClick);
+// logo.addEventListener("mousedown", onMouseDown);
+// logo.addEventListener("mouseup", onMouseUp);
+// logo.addEventListener("wheel", onMouseWheel);
+// logo.addEventListener("mouseover", onMouseOver);
+// logo.addEventListener("mouseout", onMouseOut);
+// logo.addEventListener("dragstart", onDragStart);
+// logo.addEventListener("drag", onDrag);
+// logo.addEventListener("dragend", onDragEnd);
