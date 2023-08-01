c94e9b22a8e76689293f92dfeded98b56116bd45
Wojtek Mitan <wojtekmitan91@gmail.com>
2023-08-01 13:57:35 +0200

    event values

--- SHOPING LIST/script.js
+++ SHOPING LIST/script.js
@@ -333,3 +333,33 @@
 // logo.addEventListener("dragstart", onDragStart);
 // logo.addEventListener("drag", onDrag);
 // logo.addEventListener("dragend", onDragEnd);
+
+const logo = document.querySelector("img");
+
+function onClick(e) {
+  //   console.log(e.target);
+  //   console.log(e.currentTarget);
+  //   e.target.style.backgroundColor = "red";
+  //   console.log(e.type);
+  //   console.log(e.timeStamp);
+  //   console.log(e.clientX);
+  //   console.log(e.clientY);
+  //   console.log(e.offsetX);
+  //   console.log(e.offsetY);
+  //   console.log(e.pageX);
+  //   console.log(e.pageY);
+  //   console.log(e.screenX);
+  //   console.log(e.screenY);
+}
+
+function onDrag(e) {
+  document.querySelector("h1").textContent = `X ${e.clientX} Y ${e.clientY}`;
+}
+
+logo.addEventListener("click", onClick);
+logo.addEventListener("drag", onDrag);
+
+document.querySelector("a").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("Link was clicked");
+});
